3c3001003eb12e74f040bcf25b7ccba0dd67bd0a
Mike Liang <cooldina1989@gmail.com>
2026-01-09 12:25:05 +0800

    update dag

--- dags/ingestion/eth_backfill_by_block.py
+++ dags/ingestion/eth_backfill_by_block.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.exceptions import AirflowFailException
-from airflow.operators.python import get_current_context
+from airflow.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
 from datetime import datetime
@@ -10,14 +10,16 @@ doc_md = """
 
 Image: eth-backfill
 Changes:
-0.1.1: Use KuerbenetesPodOperator
-0.1.2: 
+- 0.1.1: Use KuerbenetesPodOperator
+- 0.1.2: 
     - Add RPC env injection
     - Fix schema registry dependency
     - Insert ENV from k8s secrets
-0.1.3:
+- 0.1.3:
     - Insert static and dynamic variable
     - Trigger example: {"start_block": 24188501,"end_block": 24188600}
+- 0.1.4:
+    - add PythonOperator for input parameter validation
 """
 
 eth_infura_secret = Secret(
@@ -42,25 +44,34 @@ with DAG(
     tags=["eth-mainnet", "backfill", "ingestion"],
     doc_md = doc_md,
 ) as dag:
-
-    def get_env_vars(**context):
-        dag_run = context["dag_run"]
+    
+    # --------------------------
+    # Step1: validate start_block and end_block
+    # --------------------------
+    def validate_blocks(**context):
+        dag_run = context.get("dag_run")
         conf = dag_run.conf or {}
-
         start_block = conf.get("start_block")
         end_block = conf.get("end_block")
 
         if not start_block or not end_block:
             raise AirflowFailException("start_block and end_block are required")
+        if int(end_block) < int(start_block):
+            raise AirflowFailException("start_block must less or equal than end_block")
 
-        env = {
-            "JOB_NAME": "eth_backfill",
-            "START_BLOCK": str(start_block),
-            "END_BLOCK": str(end_block),
-        }
-        return env
+        # return conf for downstream task
+        return {"start_block": start_block, "end_block": end_block}
+
+    validate_blocks_task = PythonOperator(
+        task_id="validate_blocks",
+        python_callable=validate_blocks,
+        provide_context=True,
+    )
     
-    run_backfill = KubernetesPodOperator(
+    # --------------------------
+    # Step2: run backfill in KubernetesPodOperator
+    # --------------------------
+    run_backfill_task = KubernetesPodOperator(
         task_id="run_eth_backfill_by_block",
         name="eth-backfill",
         namespace="default",
@@ -69,5 +80,12 @@ with DAG(
         get_logs=True,
         is_delete_operator_pod=True,
         secrets=[eth_infura_secret, etherscan_secret],
-        env_vars=get_env_vars,
+        env_vars={
+            # Airflow auto generated run_id
+            "RUN_ID": "{{ run_id }}",
+            "START_BLOCK": "{{ dag_run.conf.get('start_block') }}",
+            "END_BLOCK": "{{ dag_run.conf.get('end_block') }}",
+        },
     )
+    
+    validate_blocks_task >> run_backfill_task
